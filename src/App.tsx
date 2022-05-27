50ee7c34b20a477ea09f86754e5e1a42f3723147
ChooHeonWoo <cngjsdn9@gmail.com>
2022-05-27 15:09:00 +0900

    3.2 Typing the Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import styled, { keyframes } from "styled-components";
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
