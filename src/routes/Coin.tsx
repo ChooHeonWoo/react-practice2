d1129df512b4fa7b2b972fa4364363156f2ef041
ChooHeonWoo <cngjsdn9@gmail.com>
2022-06-02 13:47:03 +0900

    4.12 Price Chart

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -222,7 +222,7 @@ function Coin() {
               <Link to={`/${coinId}/price`}>Price</Link>
             </Tab>
           </Tabs>
-          <Outlet />
+          <Outlet context={{ coinId }} />
         </>
       )}
     </Container>
